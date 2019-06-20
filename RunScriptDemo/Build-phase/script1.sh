df9bf1a97b18b06ee4e5e38f813ed4adb796fd4c
Bhavesh Tandel <bhavesh.tandel@digitas.com>
2019-06-20 20:30:20 +0530

    added demo

---
+++ RunScriptDemo/Build-phase/script1.sh
@@ -0,0 +1,9 @@
+#Step to create External run script
+#1) Target > Build Phase > + button > Click "Create new Run Script Phase"
+#2) Create External file of extension .sh (Script Programming for bash)
+#3) Run on terminal : chmod u+x <<File path of .sh file>>
+#4) Add file path in created run script phase under project build phase
+#5) Add file in project but While adding don't add it to any target.
+
+echo “ — — — — — — — — — — test run script started from external file — — — — — — — — — — “
+echo “ — — — — — — — — — — test run script stopped from external file — — — — — — — — — — “
---
+++ RunScriptDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunScriptDemo
+//
+//  Created by Bhavesh Tandel on 20/06/19.
+//  Copyright © 2019 Bhavesh Tandel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
